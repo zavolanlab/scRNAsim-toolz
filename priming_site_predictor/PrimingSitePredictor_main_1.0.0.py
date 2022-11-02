f45595a6a7b5cecd411ee1a014f8d24c5cc911ff
Robin Christen <robin.christen@stud.unibas.ch>
2022-11-02 09:56:52 +0100

    test run of file and commit via PyCharm

--- priming_site_predictor/PrimingSitePredictor_main_1.0.0.py
+++ priming_site_predictor/PrimingSitePredictor_main_1.0.0.py
@@ -1,2 +1,4 @@
 """This is supposed to be our main Program, which only implements VC code submitted by and controlled by the group
 members """
+
+print("Hello World") #test run of file and commit via PyCharm
