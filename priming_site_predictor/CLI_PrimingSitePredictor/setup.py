d3b2d323d8974be5c626579a2e839357b690c4e2
Robin Christen <robin.christen@stud.unibas.ch>
2022-11-08 11:48:12 +0100

    Rename CLI project to CLI_PrimingSitePredictor

---
+++ priming_site_predictor/CLI_PrimingSitePredictor/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name = 'CLI',
+    url = 'https://git.scicore.unibas.ch/zavolan_group/tools/priming-site-predictor/-/tree/main/CLI',
+    author = 'Robin_Christen',
+    author_email = 'robin.christen@stud.unibas.ch',
+    description = 'Command-Line Interface',
+    license = 'MIT,',
+    version = '0.1.0',
+    packages = ['pycli'],
+    entry_points = {
+        'console_scripts': [
+            'pycli = pycli.__main__:main'
+        ]
+    })
