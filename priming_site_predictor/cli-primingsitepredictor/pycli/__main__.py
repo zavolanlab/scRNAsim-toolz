886a494eeb466b6da4fdee850604557691ea2c7d
Robin Christen <robin.christen@stud.unibas.ch>
2022-11-08 15:30:24 +0100

    change

--- priming_site_predictor/cli-primingsitepredictor/pycli/__main__.py
+++ priming_site_predictor/cli-primingsitepredictor/pycli/__main__.py
@@ -1,17 +1,17 @@
 import sys
-from .classmodule import MyClass
-from .funcmodule import my_function
+#from .classmodule import MyClass
+#from .funcmodule import my_function
 
 def main():
     print('in main')
     args = sys.argv[1:]
     print('count of args :: {}'.format(len(args)))
     for arg in args:
-        print('passed argument :: {}'.format(arg)
+        print('passed argument :: {}'.format(arg))
 
-    my_function('Hello World')
-    my_object = MyClass('Robin')
-    my_object.say_name()
+    #my_function('Hello World')
+    #my_object = MyClass('Robin')
+    #my_object.say_name()
 
 if __name__ == '__main__':
     main()
