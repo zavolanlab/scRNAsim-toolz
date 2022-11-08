dcebd3052c431fbbdd700bcfd4c24a4f2deb987a
Robin Christen <robin.christen@stud.unibas.ch>
2022-11-08 10:40:29 +0100

    changes in setup and main

--- priming_site_predictor/CLI/pycli/__main__.py
+++ priming_site_predictor/CLI/pycli/__main__.py
@@ -7,7 +7,7 @@ def main():
     args = sys.argv[1:]
     print('count of args :: {}'.format(len(args)))
     for arg in args:
-        print('passed argument :: {}'.format(arg))
+        print('passed argument :: {}'.format(arg)
 
     my_function('Hello World')
     my_object = MyClass('Robin')
