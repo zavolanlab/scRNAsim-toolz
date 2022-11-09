3c90a03ca8b86ed8f53f8fe813cddc03ba89030a
Samuel Mondal <samuel.mondal@unibas.ch>
2022-11-09 07:33:08 +0100

    Adding initial LICENSE and setup

---
+++ transcript_sequence_extractor/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sequence_extractor',
+    author='Samuel Mondal',
+    url='https://git.scicore.unibas.ch/zavolan_group/tools/transcript-sequence-extractor',
+    license='MIT',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    entrypoints={
+        'console_scripts': ['run=sequence_extractor.cli:main']
+    }
+)
