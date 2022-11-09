10c3a91516f74230599d0912b624c82052c7c276
Eric Boittier <ericdavid.boittier@unibas.ch>
2022-11-09 10:46:51 +0000

    Update cdna.py

--- cdna_generator/cdna/cdna.py
+++ cdna_generator/cdna/cdna.py
@@ -16,16 +16,20 @@ class cDNA_Gen:
                  output_csv = "cDNA.csv"
                 ):
         # inputs
+        self.fasta = fasta
+        self.gtf = gtf
+        self.cpn = cpn
         self.output_fasta = output_fasta
         self.output_csv = output_csv
-        
         # variables
         self.prime_sites = []
         self.fasta_seq = ""
+        self.fasta_id = ""
+        self.copy_numbers = {}
         
-        self.main()
+        self.run()
         
-    def main(self):
+    def run(self):
         self.read_fasta()
         self.read_gtf()
         
@@ -35,22 +39,61 @@ class cDNA_Gen:
     def generate_cdna(self):
         pass
     
+    
     def read_fasta(self):
-        pass
+        fasta = open(self.fasta).readlines()
+        self.fasta_id = fasta[0]
+        print(fasta[0])
+        self.fasta_seq = "".join([_.rstrip() for _ in fasta[1:]])
+        
     
     def read_gtf(self):
-        pass
-    
+        with open(self.gtf) as gtf_file:
+            gtf_lines = gtf_file.readlines()
+            for line in gtf_lines[:1000]:
+                if not line.startswith("#"):
+                    temp_gtf = GTF_entry(line)
+                    temp_gtf.set_sequence(self.fasta_seq)
+                    self.prime_sites.append(temp_gtf)
+
     def write_fasta(self):
         pass
     
     def read_copy_numbers(self):
-        pass
+        with open(self.cpn) as cpn_file:
+            cpn_lines = cpn_file.readlines()
+            for line in cpn_lines:
+                csv = line.split(",")
+                trans_id = csv[0]
+                if trans_id:
+                    gene_id = csv[1]
+                    count = csv[2]
+                    self.copy_numbers[gene_id] = count
+                
     
     def return_output(self):
         return self.output_fasta, self.output_csv
     
 
+class GTF_entry:
+    def __init__(self, string):
+        self.string = string
+        self.values = self.string.split("\t")
+        self.start = int(self.values[3])
+        self.end = int(self.values[4])
+        self.score = float(0.5) #self.values[5]
+        self.sequence = "no sequence set"
+        self.length = self.end - self.start
+        
+    def __repr__(self):
+        return self.sequence[:10] + "..." + f" len={self.length} score={self.score}"
     
+    def set_sequence(self, full_sequence):
+        self.sequence = full_sequence[self.start:self.end]
+    
+    
+if __name__ == "__main__":
+    import argparse
+    pass
     
     
