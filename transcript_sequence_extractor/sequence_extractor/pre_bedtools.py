975227f5e8028bd124ee2052930d8d6ae42314c6
Samuel Mondal <samuel.mondal@unibas.ch>
2022-11-12 18:08:41 +0100

    Error correction

--- transcript_sequence_extractor/sequence_extractor/pre_bedtools.py
+++ transcript_sequence_extractor/sequence_extractor/pre_bedtools.py
@@ -2,14 +2,8 @@ import pandas as pd
 
 gtf = pd.read_table("Homo_sapiens.GRCh38.107.gtf.gz",skiprows=5,header=None)
 
-feat = list(gtf[8])
-superlist = []
-for x in range(len(feat)):
-    newlist = feat[x].split(";")
-    superlist.append(newlist)
 
-exons = gtf[gtf[2]=="exon"]   
-    
+exons = gtf[gtf[2]=="exon"]
 feat = list(exons[8])
 superlist = []
 idlist = []
@@ -20,9 +14,6 @@ for x in range(len(feat)):
 
 
 bed = {"chr":exons[0],"start":exons[3],"end":exons[4],"transcript_id":superlist,"score":exons[5],"strand":exons[6],"gene_id":idlist}
-
 bed = pd.DataFrame(bed)
-
 bed.to_csv("bed_file.bed",sep="\t",index=False)
-
 bed[(bed["gene_id"]=="ENSG00000160072")|(bed["gene_id"]== "ENSG00000142611")|(bed["gene_id"]=="ENSG00000232596")].to_csv("test.bed",sep="\t",index=False,header=None)
