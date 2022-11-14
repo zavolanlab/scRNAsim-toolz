1da6f1874e186550fac53b85b33edcb0b0f2432e
Samuel Mondal <samuel.mondal@unibas.ch>
2022-11-14 23:51:20 +0100

    indentation change

--- transcript_sequence_extractor/sequence_extractor/exon_concatenation.py
+++ transcript_sequence_extractor/sequence_extractor/exon_concatenation.py
@@ -2,14 +2,12 @@ def exon_concatenation:
 	fa = open("fasta.fa",'r')
 	lines = fa.readlines()
 	for x in range(int(len(lines)/2)):
-    		if x == 0:
-        		annotation = lines[0]
-        		read = lines[1]
-    		if x >= 1:
-        		if lines[2*x] == lines[2*(x-1)]:
-            			read+= lines[(2*x)+1]
-        		else:
-				return annotation
-				return read
-            			annotation = lines[2*x]
-            			read = lines[(2*x)+1]
+		if x == 0:
+			annotation = lines[0]
+			read = lines[1]
+		if x >= 1:
+			if lines[2*x] == lines[2*(x-1)]:
+				read+= lines[(2*x)+1]
+			else:
+				annotation = lines[2*x]
+				read = lines[(2*x)+1]
