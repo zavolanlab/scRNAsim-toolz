7a4489f611479e5e125662c4b7d9632fddc50028
Samuel Mondal <mondal0000@>
2022-11-15 10:25:46 +0100

    restructuring the function

--- transcript_sequence_extractor/sequence_extractor/exon_concatenation.py
+++ transcript_sequence_extractor/sequence_extractor/exon_concatenation.py
@@ -1,6 +1,7 @@
-def exon_concatenation:
-	fa = open("fasta.fa",'r')
+def exon_concatenation(filename):
+	fa = open(filename,'r')
 	lines = fa.readlines()
+	to_write_to_file = []
 	for x in range(int(len(lines)/2)):
 		if x == 0:
 			annotation = lines[0]
@@ -9,5 +10,10 @@ def exon_concatenation:
 			if lines[2*x] == lines[2*(x-1)]:
 				read+= lines[(2*x)+1]
 			else:
+				to_write_to_file.append(annotation)
+				to_write_to_file.append(read)
 				annotation = lines[2*x]
 				read = lines[(2*x)+1]
+	to_write_to_file.append(annotation)
+	to_write_to_file.append(read)
+	return to_write_to_file
