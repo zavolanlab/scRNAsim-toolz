f9ff7d4cd1ca0d40948bbf725e8c910280378cd4
Eric Boittier <ericdavid.boittier@unibas.ch>
2022-11-23 09:35:56 +0000

    [Add] setup.py example from notes

---
+++ cdna_generator/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    url='https://gitlab.com/my_user_name/my_package.git',
+    author='My Name',
+    author_email='me@email.org',
+    description='Brief package description',
+    license='MIT',
+    version='1.0.0',
+    packages=find_packages(),  # this will autodetect Python packages from the directory tree, e.g., in `code/`
+    install_requires=[],  # add here packages that are required for your package to run, including version or range of versions
+)
