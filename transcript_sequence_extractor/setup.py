12c352d6f5756187efb3458cce559e4dbc9c2c89
Samuel Mondal <mondal0000@bz-rgab01-pdm02.bioz.unibas.ch>
2022-12-13 15:30:25 +0100

    final changes to srtup.py

--- transcript_sequence_extractor/setup.py
+++ transcript_sequence_extractor/setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+project_root_dir = Path(__file__).parent.resolve()
+
+with open(project_root_dir / "requirements.txt", "r", encoding="utf-8") as _file:
+    INSTALL_REQUIRES = _file.read().splitlines()
 
 setup(
     name='sequence_extractor',
     author='Samuel Mondal',
+    author_email='samuel.mondal@unibas.ch',
     url='https://git.scicore.unibas.ch/zavolan_group/tools/transcript-sequence-extractor',
     license='MIT',
-    version='0.2.0',
+    version='0.0.1',
+    description='Extracts transcript sequences from gtf file and adds polyA tail to the output sequence',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=INSTALL_REQUIRES,
     entrypoints={
         'console_scripts': ['sequence_extractor=sequence_extractor.cli:main']
         }
