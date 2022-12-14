e1c0f64cc117efe4baea3a01b3866e79f5b5aa21
Samuel Mondal <samuel.mondal@unibas.ch>
2022-12-14 09:40:42 +0100

    Add actual tests to the test file

--- transcript_sequence_extractor/tests/test_exon_concatenation.py
+++ transcript_sequence_extractor/tests/test_exon_concatenation.py
@@ -1,3 +1,9 @@
 import pytest
-
 import exon_concatenation from exon_concatenation
+
+test_fasta_1 = "test_files/test_1.fa"
+test_fasta_2 = "test_files/test_2.fa"
+
+def test_exon_concatenation():
+    assert exon_concatenation(test_fasta_1) == expected_list_of_tuples
+    assert exon_concatenation(test_fasta_2) == expected
--- transcript_sequence_extractor/tests/test_poly_a.py
+++ transcript_sequence_extractor/tests/test_poly_a.py
@@ -1,3 +1,12 @@
 import pytest
-
 from poly_a import poly_a_generator, poly_a_addition_to_fasta_list
+
+
+
+
+def test_poly_a_generator():
+    assert poly_a_generator(exon_string) == exon_and_polya
+
+
+def test_poly_a_addition_to_fasta_list():
+    assert poly_a_addition_to_fasta_list(list_of_tuples) == manipulated_list_of_tuples
